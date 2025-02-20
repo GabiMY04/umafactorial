f4134d4991b8f5ca230657f1b6081c1c07fab066
GabiMY <gabimilenova04@gmail.com>
2025-02-20 15:35:30 +0100

    refactorizacion

--- factorial.java
+++ factorial.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 public class factorial {
-    public static void main(String []args) 
+    public static long fact(long num) 
     {
-    long num = scanInput.nextLong();
-    long fac = 1; 
-    Scanner scanInput = new Scanner(System.in);
-
-    System.out.print("Introduzca un numero: ");
-
+    long fac = 1;
     for (long i=1; i<=num; ++i) {
         fac = fac * i;
     }
-    System.out.println("El factorial de "+num+" es "+fac);
-    scanInput.close();
+    return fac;
+    }
+    public static void main(String []args) 
+    {
+        try (Scanner scanInput = new Scanner(System.in)) {
+            System.out.print("Introduzca un numero: ");
+            long num = scanInput.nextLong();
+            long fac = fact(num);
+            System.out.println("El factorial de "+num+" es "+fac);
+        }
     }
 }
